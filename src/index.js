bc776fe048521b41021d832a501d242c184ecd93
tommasosorrentino <t.sorrentino14@studenti.unisa.it>
2023-04-01 23:54:26 +0200

    changed index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Square from './tic_tac_toe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
